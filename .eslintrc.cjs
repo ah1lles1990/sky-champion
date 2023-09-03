ec5f15a7d538ca6721630feef5aea020f4e5c9b7
Igor Tsymbal <ah1lles1990@gmail.com>
2023-09-03 20:24:08 +0300

    Add vite.config.ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
         node: true
       },
       files: [
-        '.eslintrc.{js,cjs}'
+        '.eslintrc.{js,cjs}', 'vite.config.ts'
       ],
       parserOptions: {
         parser: '@typescript-eslint/parser',
